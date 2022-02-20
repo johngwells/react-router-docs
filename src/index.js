a911cfd25f0914bd4b96ffec428c0872deca8a06
John Wells <molekewl2017@gmail.com>
2022-02-19 18:00:55 -0800

    create routes for expenses & invoices

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='expenses' element={<Expenses />} />
+        <Route path='invoices' element={<Invoices />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
